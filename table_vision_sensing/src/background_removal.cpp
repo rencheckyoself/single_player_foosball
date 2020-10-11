6ae1a775dcd0c30bc0d7d3a66601ca9e85b8a3e5
Michael Rencheck <rencheck.1@live.com>
2020-10-11 12:30:56 -0500

    Added background background_removal node

---
+++ table_vision_sensing/src/background_removal.cpp
@@ -0,0 +1,89 @@
+/// \file
+/// \brief Node pull the image from the overhead camera and identify the ball's location
+/// PARAMETERS:
+/// PUBLISHES:
+///
+/// SUBSCRIBES:
+///
+/// SERIVCES:
+
+#include <vector>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/video.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+  cv::Ptr<cv::BackgroundSubtractor> pBackSub;
+  cv::Mat fgMask;
+
+public:
+  ImageConverter() : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/camera/image_rect", 1, &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    ros::NodeHandle n("~");
+
+    // intialize background subtraction
+    pBackSub = cv::createBackgroundSubtractorKNN(500, 800.0, false);
+
+    // cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    // cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+
+    // Attempt to copy the ROS image into opencv image
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_BGR2GRAY);
+
+    pBackSub->apply(cv_ptr->image, fgMask);
+
+    // Update GUI Window
+    cv::imshow("Original", cv_ptr->image);
+    cv::imshow("Mask", fgMask);
+
+
+    cv::waitKey(3);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "background_removal");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
+}
